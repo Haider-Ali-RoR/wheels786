06c31bd7e73239c47549957a67ce74cc3896f9ff
haideralidevs <haideralidevs@gmail.com>
2026-08-04 04:49:21 +0500

    Fixed linter issue
    Signed-off-by: haideralidevs <haideralidevs@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Hero from "./components/Hero";
 import Stats from "./components/Stats";
 import Services from "./components/Services";
 import Fleet from "./components/Fleet";
-import FixedRates from "./components/FixedRates";
 import WhyUs from "./components/WhyUs";
 import Testimonials from "./components/Testimonials";
 import Faq from "./components/Faq";
